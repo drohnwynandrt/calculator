0868a373e1b038f24b697b1ed2f6622a87216e9d
drohnwynandrt <dw.ramdattewari@gmail.com>
2019-05-06 14:54:53 +0200

    Updated the buttons and added data-tags to identify them in Javascript.

---
+++ calculator.js
@@ -0,0 +1,8 @@
+const numberButtons = document.querySelectorAll ('[data-number]')
+const operationButtons = document.querySelectorAll ('[data-operation]')
+const equalsButton = document.querySelector ('data-equals]')
+const deleteButton = document.querySelector ('data-delete]')
+const allClearButton = document.querySelector ('data-allclear]')
+const previousOperandTextElement = document.querySelector ('data-previousoperand]')
+const currentOperandTextElement = document.querySelector ('data-currentoperand]')
+
