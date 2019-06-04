aa75beafceec8eb9eecfae824e8052588d5dbddb
drohnwynandrt <dw.ramdattewari@gmail.com>
2019-06-04 09:01:56 +0200

    Changed the name of the HTML file to create a live demo on github

--- calculator.js
+++ calculator.js
@@ -1,3 +1,39 @@
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+    this.previousOperandTextElement = previousOperandTextElement
+    this.currentOperandTextElement = currentOperandTextElement
+    this.clear()
+}
+
+    clear() {
+        this.currentOperand = ''
+        this.previousOperand = ''
+        this.operation = undefined
+
+    }
+
+    delete() {
+
+    }
+
+    appendNumber(number){
+
+    }
+
+    chooseOperation (operation) {
+
+    }
+
+    compute(){
+
+    }
+    
+    updateDisplay() {
+
+    }
+
+}
+
 const numberButtons = document.querySelectorAll ('[data-number]')
 const operationButtons = document.querySelectorAll ('[data-operation]')
 const equalsButton = document.querySelector ('data-equals]')
@@ -6,3 +42,8 @@ const allClearButton = document.querySelector ('data-allclear]')
 const previousOperandTextElement = document.querySelector ('data-previousoperand]')
 const currentOperandTextElement = document.querySelector ('data-currentoperand]')
 
+const calculator = new Calculator (previousOperandTextElement, currentOperandTextElement)
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () =>
+})
